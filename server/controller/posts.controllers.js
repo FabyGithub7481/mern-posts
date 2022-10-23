393e2518885c86236ca087cbe9811b8e529555fb
FabyGithub7481 <gabogabidia@hotmail.com>
2022-10-22 21:17:10 -0500

    sin errores sin error handler

--- server/controller/posts.controllers.js
+++ server/controller/posts.controllers.js
@@ -2,7 +2,7 @@ import Post from "../models/Posts.js";
 
 export const getPosts = async (req, res) => {
   const posts = await Post.find({});
-  res.send("Get posts");
+  res.send(posts);
 };
 export const createPosts = async (req, res) => {
   //console.log(req.body);ç
@@ -12,6 +12,28 @@ export const createPosts = async (req, res) => {
   await newPost.save();
   return res.json(newPost);
 };
-export const updatePost = (req, res) => res.send("update posts");
-export const deletePost = (req, res) => res.send("delete posts");
-export const getPost = (req, res) => res.send("Get post");
+export const updatePost = async (req, res) => {
+  /* console.log(req.params)
+     console.log(req.body) */
+  const postUpdated = await Post.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  //console.log(postUpdated)
+
+  res.send(postUpdated);
+};
+export const deletePost = async (req, res) => {
+  const postDelete = await Post.findByIdAndDelete(req.params.id);
+  if (!postDelete) {
+    res.sendStatus(404).send("Post not found");
+  }
+  res.sendStatus(204);
+  //res.send(postDelete);
+};
+export const getPost = async (req, res) => {
+  const postFound = await Post.findById(req.params.id);
+  if (!postFound) {
+    res.sendStatus(404).send("Post not found");
+  }
+  res.send(postFound);
+};
